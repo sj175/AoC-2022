bd1e94d09e6bd77a5defee717091947bb8055467
Sam Jones <sam.jones@gocity.com>
2022-12-12 19:15:08 +0000

    draw shit because Im stupid

--- day9.py
+++ day9.py
@@ -50,10 +50,10 @@ def validate_rules(h: Rope, t: Rope) -> bool:
 def get_new_t(h: Rope, t: Rope) -> Rope:
     diff = h - t
     if (abs(diff.x) == 2) and (diff.y == 0):
-        return Rope(t.x + diff.x//2, t.y)
+        return Rope(t.x + diff.x // 2, t.y)
 
     elif (abs(diff.y) == 2) and (diff.x == 0):
-        return Rope(t.x, t.y + diff.y//2)
+        return Rope(t.x, t.y + diff.y // 2)
 
     elif (abs(diff.x) == 2) and (abs(diff.y) == 1):
         return Rope(t.x + diff.x // 2, t.y + diff.y)
@@ -63,7 +63,7 @@ def get_new_t(h: Rope, t: Rope) -> Rope:
 
     else:
         print("diff: ", diff)
-        raise Exception("you fucked up")
+        raise Exception("I don't know what the next move should be")
 
 
 def main() -> int:
@@ -87,6 +87,24 @@ def part1() -> int:
     return main()
 
 
+DRAWING_GRID = [-5, 5]
+
+
+def draw_grid(ropes):
+    output = ""
+    rope_ords = list(map(lambda r: (r.x, r.y), ropes))
+
+    for i in range(DRAWING_GRID[1], DRAWING_GRID[0], -1):
+        for j in range(DRAWING_GRID[0], DRAWING_GRID[1]):
+            if (j, i) in rope_ords:
+                output += str(rope_ords.index((j, i)))
+            else:
+                output += "."
+        output += "\n"
+
+    print(output)
+
+
 def part2() -> int:
     moves = filter(len, read_input().split("\n"))
     ropes = []
@@ -95,19 +113,21 @@ def part2() -> int:
     t_destinations = {ropes[9]}
 
     for move in moves:
+        draw_grid(ropes)
         dir = move.split(" ")[0]
         length = int(move.split(" ")[1])
         for _ in range(length):
             ropes[0] = ropes[0] + UNIT_DIRECTIONS[dir]
             for i in range(9):
-                if not validate_rules(ropes[i], ropes[i+1]):
-                    ropes[i+1] = get_new_t(ropes[i], ropes[i+1])
+                if not validate_rules(ropes[i], ropes[i + 1]):
+                    ropes[i + 1] = get_new_t(ropes[i], ropes[i + 1])
                     if i == 8:
-                        t_destinations.add(ropes[i+1])
+                        t_destinations.add(ropes[i + 1])
+        print(move)
 
     return len(t_destinations)
 
 
 if __name__ == "__main__":
-    # print(part1())
+    print(part1())
     print(part2())
