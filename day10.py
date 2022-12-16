b2eff100afa4a34f45445f4eb8a2563dec9d5e6a
Sam Jones <sam.jones@gocity.com>
2022-12-16 23:19:16 +0000

    day10 part 2

--- day10.py
+++ day10.py
@@ -15,6 +15,14 @@ def check_signal_strength(signal_strengths, cycle, x):
         signal_strengths.append(cycle * x)
 
 
+SCREEN_BUFFER = [["."] * 40 for _ in range(6)]
+
+
+def draw(register, cycle):
+    if (register - 1) <= (cycle - (40 * (cycle // 40)) - 1) <= (register + 1):
+        SCREEN_BUFFER[(cycle - 1) // 40][(cycle - 1) % 40] = "#"
+
+
 def main(the_input) -> int:
     instructions = filter(len, the_input.split("\n"))
     cycle = 0
@@ -25,12 +33,15 @@ def main(the_input) -> int:
         if instr.split(" ")[0] == "noop":
             cycle += 1
             check_signal_strength(signal_strengths, cycle, x)
+            draw(x, cycle)
 
         elif instr.split(" ")[0] == "addx":
             cycle += 1
             check_signal_strength(signal_strengths, cycle, x)
+            draw(x, cycle)
             cycle += 1
             check_signal_strength(signal_strengths, cycle, x)
+            draw(x, cycle)
             x += int(instr.split(" ")[1])
 
         else:
@@ -43,10 +54,15 @@ def part1() -> int:
     return main(read_input())
 
 
-def part2() -> int:
-    return main(read_input())
+def part2():
+    main(read_input())
 
 
 if __name__ == "__main__":
-    print(part1())
-    print(part2())
+    # print(part1())
+    part2()
+    for line in SCREEN_BUFFER:
+        output = ""
+        for char in line:
+            output += char
+        print(output)
